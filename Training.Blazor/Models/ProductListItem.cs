36bd52198b081eb6b8c155a7ca13c5b2302c3de8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-09-22 15:07:17 +0200

    Add Product Validation with DataAnnotations

--- Training.Blazor/Models/ProductListItem.cs
+++ Training.Blazor/Models/ProductListItem.cs
@@ -1,8 +1,29 @@
-﻿namespace Training.Blazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Training.Blazor.Models;
 
 public sealed class ProductListItem : BaseEntity
 {
-   
+    [Required(ErrorMessage = "Nazwa jest wymagana"), StringLength(20, MinimumLength = 3)]
+    [RegularExpression(@"^S.*", ErrorMessage = "Nazwa powinna zaczynac od litery S")]
     public string Name { get; set; }
+
+    [Range(0.01, 1000)]
+    //[CustomValidation()]
+    [Currency]
     public decimal Price { get; set; }
+}
+
+public class CurrencyAttribute : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        if (decimal.TryParse(value.ToString(), out decimal currency))
+        {
+            return true;
+
+        }
+
+        return false;
+    }
 }
