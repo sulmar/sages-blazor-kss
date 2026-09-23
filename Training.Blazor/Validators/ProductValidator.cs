9bc350ca876da7378e232f1dd7e3e6c5f02416f2
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2026-09-23 10:57:32 +0200

    Add Cascade(CascadeMode.Stop)

--- Training.Blazor/Validators/ProductValidator.cs
+++ Training.Blazor/Validators/ProductValidator.cs
@@ -7,7 +7,7 @@ public class ProductValidator : AbstractValidator<ProductListItem>
 {
     public ProductValidator()
     {
-        RuleFor(x => x.Name).NotEmpty().Length(3, 20).Matches(@"^S.*").WithName("Nazwa");
+        RuleFor(x => x.Name).Cascade(CascadeMode.Stop).NotEmpty().Length(3, 20).Matches(@"^S.*").WithName("Nazwa");
         RuleFor(x => x.Price).GreaterThan(0).LessThanOrEqualTo(1000).WithName("Cena");
     }
 }
